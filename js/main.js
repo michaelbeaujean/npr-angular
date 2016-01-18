cc884187cb8a0e0bd5c7d0ea130af8ef583a2e92
System Administrator <root@hgpnapmb-h03336.home>
2016-01-18 14:21:34 -0500

    returning NPR data and appending to DOM

--- js/main.js
+++ js/main.js
@@ -1,13 +1,36 @@
-var app = angular.module("nprPlayer", []);
+var app = angular.module("nprPlayer", []),
+		apiKey = 'MDIwMzkwMTEwMDE0NDEzOTAwNDU0Njk2NQ001',
+		nprUrl = 'http://api.npr.org/query?id=61&fields=relatedLink,title,byline,text,audio,image,pullQuote,all&output=JSON';
 
-app.controller('PlayerController', ['$scope', function($scope) {
+app.directive('nprLink', function() {
+	return {
+		restrict: 'EA',
+		require: ['^ngModel'],
+		replace: true,
+		scope: {
+			ngModel: '=',
+			play: '&'
+		},
+		templateUrl: 'views/nprListItem.html',
+		link: function(scope, ele, attr) {
+			scope.duration = scope.ngModel.audio[0].duration.$text;
+		}
+	}
+});		
 
-	$scope.playing = false;
-	$scope.audio = document.createElement('audio');
-	$scope.audio.src = ('media/npr.mp4');
+app.controller('PlayerController', function($scope, $http) {
 
-	$scope.play = function() {
-		$scope.audio.play();
+	var audio = document.createElement('audio');
+	$scope.audio = audio;
+
+	$scope.play = function(program) {
+		if ($scope.playing)
+		{
+			$scope.audio.pause();
+		}
+		var url = program.audio[0].format.mp4.$text;
+		audio.src = url;
+		audio.play();
 		$scope.playing = true;
 	};
 
@@ -16,12 +39,21 @@ app.controller('PlayerController', ['$scope', function($scope) {
 		$scope.playing = false;
 	};
 
+	$http({
+		method: 'JSONP',
+		url: nprUrl + '&apiKey=' + apiKey + '&callback=JSON_CALLBACK'
+	}).success(function(data, status) {
+		$scope.programs = data.list.story;
+	}).error(function(data, status) {
+
+	});		
+
 	$scope.audio.addEventListener('ended', function() {
-		$scope.apply(function() {
+		$scope.$apply(function() {
 			$scope.stop();
 		});
 	});
-}]);
+});
 
 app.controller('RelatedController', ['$scope', function($scope) {
 
